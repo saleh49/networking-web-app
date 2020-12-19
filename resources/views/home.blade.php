0dba77660618631902f1c5b00655016946e700ba
Saleh Khan <saleh.lab49@gmail.com>
2020-12-19 21:18:03 +0530

    Initial Commit Main

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+    	<h1>Welcome, you are logged in</h1>
+    </div>
+</div>
+@endsection
